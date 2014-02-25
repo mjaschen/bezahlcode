d9926419e18467a20b68b5eb722663f3a15e44b5
Marcus Jaschen <mail@marcusjaschen.de>
2014-02-25 22:30:49 +0100

    New Type: SEPA Debit

---
+++ src/MarcusJaschen/BezahlCode/Type/SepaDebit.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * BezahlCode SEPA Debit Type
+ *
+ * PHP version 5
+ *
+ * @category  BezahlCode
+ * @package   Type
+ * @author    Marcus Jaschen <mail@marcusjaschen.de>
+ * @copyright 2013 r03.org
+ * @license   http://www.opensource.org/licenses/mit-license MIT License
+ * @link      http://r03.org/
+ */
+
+namespace MarcusJaschen\BezahlCode\Type;
+
+/**
+ * BezahlCode SEPA Debit Type
+ *
+ * @category BezahlCode
+ * @package  Type
+ * @author   Marcus Jaschen <mail@marcusjaschen.de>
+ * @license  http://www.opensource.org/licenses/mit-license MIT License
+ * @link     http://r03.org/
+ */
+class SepaTransfer extends AbstractType
+{
+    /**
+     * @var array
+     */
+    protected $params = array(
+        'name'            => null,
+        'iban'            => null,
+        'bic'             => null,
+        'amount'          => null,
+        'reason'          => null,
+        'reason1'         => null,
+        'reason2'         => null,
+        'reason3'         => null,
+        'reason4'         => null,
+        'reason5'         => null,
+        'reason6'         => null,
+        'reason7'         => null,
+        'reason8'         => null,
+        'reason9'         => null,
+        'reason10'        => null,
+        'reason11'        => null,
+        'reason12'        => null,
+        'reason13'        => null,
+        'reason14'        => null,
+        'reason15'        => null,
+        'currency'        => null,
+        'executiondate'   => null,
+        'separeference'   => null,
+        'creditorid'      => null,
+        'mandateid'       => null,
+        'dateofsignature' => null,
+    );
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->authority = 'singlepaymentsepa';
+    }
+}
